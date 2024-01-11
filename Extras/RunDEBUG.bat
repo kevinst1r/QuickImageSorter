6548a577034f397eedda3d8dc2f11549f54bf793
Kevin Stoner <131720469+kevinst1r@users.noreply.github.com>
2024-01-10 18:23:00 -0600

    Add files via upload

---
+++ Extras/RunDEBUG.bat
@@ -0,0 +1,24 @@
+@echo off
+:runscript
+cd ..
+python "main.py"
+cd "Extras"
+if %ERRORLEVEL% NEQ 0 (
+    echo An error occurred. Press any key to return to the menu...
+    pause > nul
+)
+
+:menu
+cls
+echo.
+echo 1. Run the Python script again
+echo 2. Exit
+echo.
+set /p choice="Enter your choice (1 or 2): "
+
+if "%choice%"=="1" goto runscript
+if "%choice%"=="2" exit
+
+echo Invalid choice
+pause
+goto menu
